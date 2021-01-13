bc961044809ee755f3f376b2d2ce3328cd3f8ac2
Sandro Huber <sandro@sh-digital.ch>
2021-01-13 22:22:50 +0100

    settings

--- umfrage/settings.py
+++ umfrage/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['https://www.sh-digital.ch/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
