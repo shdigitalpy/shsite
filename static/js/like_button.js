b229b932e733052965227efd6ac0642e5fc33d47
Sandro Huber <sandro@sh-digital.ch>
2021-01-18 17:52:23 +0100

    layout changes

--- static/js/like_button.js
+++ static/js/like_button.js
@@ -1,25 +1,7 @@
-'use strict';
+import React from "react";
+import ReactDOM from "react-dom";
+import Text from "react-text-typing";
 
-const e = React.createElement;
+const App = () => <Text text="Example Text" showBlink={true} component="h1" />;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<App />, document.getElementById("root"));
